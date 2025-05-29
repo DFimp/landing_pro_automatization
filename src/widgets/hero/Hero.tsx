02eb0be1883c3dd96eed8e2c5f8a1a00436ae14f
Lxg1c <teran_2005@mail.ru>
2025-05-29 13:27:35 +0300

    initialized hero section

---
+++ src/widgets/hero/Hero.tsx
@@ -0,0 +1,32 @@
+import Button from "@/shared/ui/button/Button";
+import Image from "next/image";
+
+const Hero = () => {
+    return(
+        <div className="hero mt-10">
+            <div className='hero__container container flex justify-between items-center'>
+                <div className="space-y-9">
+                    <h1 className='text-h3 font-semibold'>
+                        Ваш эксперт по amoCRM
+                    </h1>
+
+                    <h2 className='text-h3 font-regular max-w-140'>
+                        от консультации в решении простых задач до полной автоматизации бизнес-процессов.
+                    </h2>
+                    <div className='w-90'>
+                        <Button text="Консультация" variant='secondary' />
+                    </div>
+                </div>
+
+                <Image
+                    src='/laptop.svg'
+                    alt='Laptop'
+                    width={580}
+                    height={464}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
