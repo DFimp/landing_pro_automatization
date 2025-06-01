d950d7fb88f484d57c43901725c1a8a0e88a27ee
Lxg1c <teran_2005@mail.ru>
2025-06-01 22:58:36 +0300

    finished page

---
+++ src/app/widgets/page.tsx
@@ -0,0 +1,34 @@
+import IndustryCards from "@/widgets/industry/IndustryCards";
+import FeaturesList from "@/widgets/feature/FeatureList";
+import StickyInfo from "@/shared/ui/stickyInfo/StickyInfo";
+import WorkSteps from "@/widgets/workSteps/WorkSteps";
+import Card from "@/shared/ui/card/Card";
+
+
+export default function Widgets() {
+    const title = "Управление продажами с нуля: комплексное внедрение amoCRM";
+
+    return (
+        <main className="widgets py-20">
+            <div className="widgets__container container space-y-20">
+                <h2 className="text-h2 font-bold max-w-4xl">
+                    {title}
+                </h2>
+
+                <StickyInfo
+                    text="amoCRM предлагает широкий спектр функций, которые помогают управлять продажами,
+                    автоматизировать процессы и улучшать взаимодействие с клиентами."
+                />
+
+
+                <FeaturesList />
+
+                <IndustryCards />
+
+                <WorkSteps />
+
+                <Card />
+            </div>
+        </main>
+    );
+}
