754191616b2cc8a00b91c7ec3a6a261901939e0a
Lxg1c <teran_2005@mail.ru>
2025-06-02 23:50:02 +0300

    add variant

--- src/widgets/process/ui/ProcessItem.tsx
+++ src/widgets/process/ui/ProcessItem.tsx
@@ -1,31 +1,38 @@
-// components/ProcessItem.tsx
 import Image from "next/image";
 import AnimatedListItem from "@/enteties/process/ui/AnimatedListItem";
+import React, { ReactNode } from "react";
 
 interface ProcessItemProps {
     direction: "left" | "right";
-    imageSrc: string;
-    title: string;
-    description: string;
+    imageSrc?: string;
+    alt?: string;
+    title?: string;
+    description: ReactNode;
     offset?: string;
+    style?: React.CSSProperties;
+    className?: string;
 }
 
 export const ProcessItem = ({
                                 direction,
                                 imageSrc,
+                                alt = "Этап процесса",
                                 title,
                                 description,
-                                offset = ""
+                                offset = "",
+                                style,
+                                className = "",
                             }: ProcessItemProps) => {
     return (
         <AnimatedListItem
             direction={direction}
-            className={`flex items-center gap-10 ${offset}`}
+            className={`flex items-center gap-10 ${offset} ${className}`}
+            style={style}
         >
-            <Image src={imageSrc} alt='process image' width={150} height={150} />
-            <div className='content max-w-[500px] space-y-4'>
-                <h3 className='text-h5 font-semibold'>{title}</h3>
-                <p className="text-h7 font-normal">{description}</p>
+            {imageSrc && <Image src={imageSrc} alt={alt} width={150} height={150} />}
+            <div className='content max-w-[600px] space-y-4'>
+                {title && <h3 className='text-h5 font-semibold'>{title}</h3>}
+                {description}
             </div>
         </AnimatedListItem>
     );
