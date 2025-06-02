1a6f85d20586ef01d1e31261046ecbd7acb6bb47
Lxg1c <teran_2005@mail.ru>
2025-06-02 23:49:43 +0300

    refactored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import Hero from "@/widgets/hero/Hero";
-import Card from "@/shared/ui/card/Card";
-import {Services} from "@/widgets/services/Services";
+import HomeHero from "@/widgets/homeHero/homeHero";
+import ConsultCard from "@/shared/ui/ConsultCard/ConsultCard";
+import {TitleArrow} from "@/shared/ui/titleArrow/TitleArrow";
 import ProcessList from "@/widgets/process/ProcessList";
 
 
 export default function Home() {
   return (
       <main className="home mb-25 pt-15 space-y-20">
-          <Hero />
-          <Services
+          <HomeHero />
+          <TitleArrow
               text="Чем мы можем помочь вашему бизнесу"
               link='/'
           />
           <ProcessList />
-          <Card />
+          <ConsultCard />
       </main>
   );
 }
