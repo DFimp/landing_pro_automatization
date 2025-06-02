cdf06fdc417f86b64ccd38a09161e246d7497b45
Lxg1c <teran_2005@mail.ru>
2025-06-02 23:49:21 +0300

    add variant

--- src/shared/ui/stickyInfo/StickyInfo.tsx
+++ src/shared/ui/stickyInfo/StickyInfo.tsx
@@ -1,12 +1,15 @@
 type StickyInfoProps = {
     text: string
+    left?: boolean
 }
 
-export default function StickyInfo({text}: StickyInfoProps) {
+export default function StickyInfo({text, left = true}: StickyInfoProps) {
     return (
         <div className="sticky-info h-10">
             <div className="relative max-w-6xl">
-                <div className="absolute -left-50 pl-50 bg-blue text-white text-h4 font-normal py-4 px-10 pr-32 rounded-r-full max-w-1250">
+                <div
+                    className={`absolute bg-blue text-white text-h4 font-normal py-4 px-10 pr-32  max-w-1250 
+                    ${left ? "-left-50 rounded-r-full pl-50" : "-right-50 rounded-l-full pr-50"}`}>
                     {text}
                 </div>
             </div>
