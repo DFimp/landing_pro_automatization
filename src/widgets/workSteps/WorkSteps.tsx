5f9e83c27a0b0c16b43bab866b9cbd449d120516
Lxg1c <teran_2005@mail.ru>
2025-06-02 23:49:36 +0300

    moved data and refactored

--- src/widgets/workSteps/WorkSteps.tsx
+++ src/widgets/workSteps/WorkSteps.tsx
@@ -1,54 +1,12 @@
 'use client';
 
-import { useState, useEffect, useRef } from "react";
-import Image from "next/image";
-import { Services } from "@/widgets/services/Services";
+import {useEffect, useRef } from "react";
+import { TitleArrow } from "@/shared/ui/titleArrow/TitleArrow";
+import {Card} from "@/shared/ui/Card/Card";
+import {steps} from "@/widgets/workSteps/const";
 
-const steps = [
-    {
-        content: "Анализ и интервью с ключевыми лицами компании для составления ТЗ",
-        description: [
-            "Общее описание компании: сфера деятельности, структура, количество сотрудников.",
-            "Текущие бизнес-процессы: как сейчас осуществляется управление продажами, маркетингом, взаимодействием с клиентами.",
-            "Проблемы и задачи: выявленные проблемы в текущих процессах (например, ручной ввод данных, потеря информации, сложности в аналитике)."
-        ],
-        result: "готовое ТЗ на внедрение, стоимость, сроки"
-    },
-    {
-        content: "Базовая настройка",
-        description: [
-            "Настройка пользователей и прав доступа.",
-            "Настройка воронки продаж (канбан).",
-            "Настроим поля сделок и контактов.",
-            "Подключим телефонию и мессенджеры.",
-            "Подключим все источники сделок."
-        ],
-        result: "менеджеры могут работать в системе."
-    },
-    {
-        content: "Автоматизация рутинных действий",
-        description: [
-            "На основе обратной связи от сотрудников составим план автоматизации.",
-            "Внедрим документооборот.",
-            "Автоматизируем постановку задач и автоматическую смену статусов.",
-            "Автоматические касания клиентов.",
-            "Автоматическое формирование ссылок на оплату.",
-            "Внедрим любые виджеты."
-        ],
-        result: "автоматизированный отдел продаж, который не тратит время на рутину"
-    },
-    {
-        content: "Обучение сотрудников",
-        description: [
-            "Покажем менеджерам как работать в CRM от получения заявки до подписания актов",
-            "Научим руководителей контролировать менеджеров и получать любую информацию из CRM"
-        ],
-        result: "менеджеры могут работать в системе."
-    },
-];
 
 export default function WorkSteps() {
-    const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
     const scrollRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -71,74 +29,25 @@ export default function WorkSteps() {
 
     return (
         <>
-            <Services text="Как мы работаем?" link="/" variant='outline'/>
+            <TitleArrow text="Как мы работаем?" link="/" variant='outline' />
 
-            <div className="w-full overflow-hidden">
+            <div className="w-full overflow-hidden mb-20 mt-20">
                 <div
                     ref={scrollRef}
                     className="overflow-hidden scrollbar-none"
                 >
                     <ul className="flex gap-4 snap-x snap-mandatory pr-4">
-                        {steps.map((step, index) => {
-                            const isExpanded = expandedIndex === index;
-
-                            return (
-                                <li
-                                    key={index}
-                                    onClick={() =>
-                                        setExpandedIndex(isExpanded ? null : index)
-                                    }
-                                    className={`
-                                        snap-start shrink-0 bg-black text-white rounded-4xl px-10 py-10
-                                        flex flex-col justify-between relative cursor-pointer
-                                        transition-all duration-500 ease-in-out min-w-[360px] will-change-transform
-                                        ${isExpanded ? "max-h-[800px]" : "max-h-[270px]"}
-                                        overflow-hidden
-                                    `}
-                                >
-                                    <div>
-                                        <h4 className="text-h4 font-bold">
-                                            Шаг - {index + 1}.
-                                        </h4>
-                                        <p className="font-semibold text-h5 max-w-62.5">
-                                            {step.content}
-                                        </p>
-
-                                        {!isExpanded && (
-                                            <button className="absolute bottom-10 right-8">
-                                                <Image
-                                                    src="/arrow btn.svg"
-                                                    alt="Кнопка"
-                                                    width={55}
-                                                    height={55}
-                                                />
-                                            </button>
-                                        )}
-                                    </div>
-
-                                    {step.description && (
-                                        <div
-                                            className={`
-                                                transition-all duration-500 ease-in-out mt-8
-                                                ${isExpanded
-                                                ? "opacity-100"
-                                                : "opacity-0"}
-                                            `}
-                                        >
-                                            <ul className="max-w-60 space-y-5 list-disc list-inside">
-                                                {step.description.map((item, i) => (
-                                                    <li key={i}>{item}</li>
-                                                ))}
-                                            </ul>
-
-                                            <p className="mt-4 max-w-60">
-                                                <span className="text-blue">В результате:</span> {step.result}.
-                                            </p>
-                                        </div>
-                                    )}
-                                </li>
-                            );
-                        })}
+                        {steps.map((step, index) => (
+                            <Card
+                                variant='expandable'
+                                key={index}
+                                index={index}
+                                content={step.content}
+                                description={step.description}
+                                result={step.result}
+                                isFirst={index === 0}
+                            />
+                        ))}
                     </ul>
                 </div>
             </div>
