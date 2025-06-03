fa7cde130691a50e75f6ea3b9c569b81bc10ab9b
Lxg1c <teran_2005@mail.ru>
2025-06-03 21:06:44 +0300

    add gap

--- src/widgets/home/homeServiceSection/HomeServiceSection.tsx
+++ src/widgets/home/homeServiceSection/HomeServiceSection.tsx
@@ -3,12 +3,12 @@ import ProcessList from "@/widgets/home/homeServiceSection/ui/ProcessList";
 
 export function HomeServicesSection() {
     return (
-        <>
+        <section className='flex flex-col gap-20'>
             <TitleArrow
                 text="Чем мы можем помочь вашему бизнесу"
                 link="/"
             />
             <ProcessList />
-        </>
+        </section>
     );
 }
