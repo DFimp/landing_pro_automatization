88d47c69bac55f001bed6b010ac46130f4946a03
Lxg1c <teran_2005@mail.ru>
2025-06-03 11:54:16 +0300

    add classname prop

--- src/shared/ui/titleArrow/models.ts
+++ src/shared/ui/titleArrow/models.ts
@@ -1,5 +1,6 @@
-export type ServicesProps = {
+export type TittleArrowProps = {
     text: string;
     variant?: "primary" | "secondary" | "outline";
     link: string;
+    className?: string;
 };
