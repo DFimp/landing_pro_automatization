6efa939c3e69b13e29b2763f05e4a1491cdebc70
Lxg1c <teran_2005@mail.ru>
2025-06-03 20:28:53 +0300

    initialized

---
+++ src/widgets/accompaniment/index.ts
@@ -0,0 +1,5 @@
+export * from "./AccompanimentHelpSection/AccompanimentHelpSection";
+export * from "./AccompanimentProblemsSection/AccompanimentProblemsSection";
+export * from "./AccompanimentHeroSection/AccompanimentHeroSection";
+export * from "./AccompanimentScrumSection/AccompanimentScrumSection";
+export * from "./AccompanimentTariffsSection/AccompanimentTariffsSection";
