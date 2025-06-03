0a3f7ae5577b17c27cb6fbf2977cb26ef014a9c1
Lxg1c <teran_2005@mail.ru>
2025-06-03 12:16:23 +0300

    add animation

--- src/widgets/whyChooseUs/WhyChooseUs.tsx
+++ src/widgets/whyChooseUs/WhyChooseUs.tsx
@@ -1,84 +1,102 @@
+'use client'
+
 import Image from 'next/image';
-import React from 'react';
-import {Card} from "@/shared/ui/Card/Card";
+import React, { useRef } from 'react';
+import { Card } from "@/shared/ui/Card/Card";
+import { motion, useInView } from 'framer-motion';
 
 const WhyChooseUs = () => {
+    const amoRef = useRef(null);
+    const isInView = useInView(amoRef, {
+        threshold: 0.5, // половина элемента в зоне видимости
+        once: true
+    });
+
     return (
         <section>
-            <div className="text-center container">
+            <div className="text-center container overflow">
                 <h2 className='font-bold text-h2 mb-6'>Почему выбирают нас?</h2>
-                <div className='mx-auto max-w-3xl px-4'> {/* Добавлен контейнер с max-width */}
+                <div className='mx-auto max-w-3xl px-4'>
                     <p className='text-h5 font-normal mb-6'>
                         Автоматизация становится умнее с каждым днем —
-                        <span className='font-medium text-h4'>Мы</span> внедряем самые актуальные инструменты, чтобы
-                        <span className='font-medium text-h4'> Вы</span> оставались впереди конкурентов.
+                        <span className='font-medium text-h4'> Мы </span>
+                        внедряем самые актуальные инструменты, чтобы
+                        <span className='font-medium text-h4'> Вы </span>
+                        оставались впереди конкурентов.
                     </p>
                 </div>
             </div>
 
-            {/* Первая строка с карточками слева и AmoCRM справа */}
+            {/* Первая строка с карточками и AmoCRM справа */}
             <div className='mb-12 relative'>
-                <div>
-                    <div className='flex gap-20 container'>
-                        <Card
-                            title="Опыт"
-                            content={
-                                <p>
-                                    Более <span className='text-blue text-6xl'>100</span> успешных проектов
-                                </p>
-                            }
-                        />
-
-                        <Card
-                            title="Опыт"
-                            content={
-                                <p>
-                                    Более <span className='text-blue text-6xl'>100</span> успешных проектов
-                                </p>
-                            }
-                        />
-                    </div>
+                <div className='flex gap-20 container'>
+                    <Card
+                        title="Опыт"
+                        content={
+                            <p>
+                                Более <span className='text-blue text-6xl'>100</span> успешных проектов
+                            </p>
+                        }
+                    />
+                    <Card
+                        title="Опыт"
+                        content={
+                            <p>
+                                Более <span className='text-blue text-6xl'>100</span> успешных проектов
+                            </p>
+                        }
+                    />
                 </div>
 
-                <Image
-                    src='/amo.svg'
-                    alt="AmoCRM"
-                    width={575}
-                    height={310}
-                    className='absolute  top-0 right-0'
-                />
+                <motion.div
+                    initial={{ x: 300, opacity: 0 }}
+                    animate={isInView ? { x: 0, opacity: 1 } : {}}
+                    transition={{ duration: 0.8, ease: 'easeOut' }}
+                    className='absolute top-0 right-0 overflow-x-hidden'
+                >
+                    <Image
+                        src='/amo.svg'
+                        alt="AmoCRM"
+                        width={575}
+                        height={310}
+                    />
+                </motion.div>
             </div>
 
-            {/* Вторая строка с карточками справа и rm слева */}
-            <div className='relative'>
-                <div className='flex'>
-                    <div className='flex justify-end gap-20 container'>
-                        <Card
-                            title="Опыт"
-                            content={
-                                <p>
-                                    Более <span className='text-blue text-6xl'>100</span> успешных проектов
-                                </p>
-                            }
-                        />
-
-                        <Card
-                            title="Опыт"
-                            content={
-                                <p>
-                                    Более <span className='text-blue text-6xl'>100</span> успешных проектов
-                                </p>
-                            }
-                        />
-                    </div>
+            {/* Вторая строка с карточками и RM слева */}
+            <div className='relative' ref={amoRef}>
+                <div className='flex justify-end gap-20 container'>
+                    <Card
+                        title="Опыт"
+                        content={
+                            <p>
+                                Более <span className='text-blue text-6xl'>100</span> успешных проектов
+                            </p>
+                        }
+                    />
+                    <Card
+                        title="Опыт"
+                        content={
+                            <p>
+                                Более <span className='text-blue text-6xl'>100</span> успешных проектов
+                            </p>
+                        }
+                    />
                 </div>
-                <Image
-                    src='/rm.svg'
-                    alt="AmoCRM"
-                    width={575}
-                    height={310}
-                    className='absolute  top-0 left-0'
-                />
+
+                <motion.div
+                    initial={{ x: -300, opacity: 0 }}
+                    animate={isInView ? { x: 0, opacity: 1 } : {}}
+                    transition={{ duration: 0.8, ease: 'easeOut' }}
+                    className='absolute top-0 left-0'
+                >
+                    <Image
+                        src='/rm.svg'
+                        alt="RM"
+                        width={575}
+                        height={310}
+                    />
+                </motion.div>
             </div>
         </section>
     );
