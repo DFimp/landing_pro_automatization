27f08f75d423f15973dc69e362f0367275544212
Lxg1c <teran_2005@mail.ru>
2025-06-03 21:07:10 +0300

    moved sticky info

--- src/widgets/analytic/analyticsHeroSection/AnalyticsHeroSection.tsx
+++ src/widgets/analytic/analyticsHeroSection/AnalyticsHeroSection.tsx
@@ -6,17 +6,6 @@ export function AnalyticsHeroSection() {
             <h2 className="text-h2 font-bold max-w-4xl">
                 Аудит и предпроектная подготовка
             </h2>
-
-            <StickyInfo text="Начнём проект с надёжного фундамента" />
-
-            <h4 className="font-normal text-h5 max-w-135">
-                Мы анализируем текущее состояние вашего бизнеса, выявляем проблемы и разрабатываем решения,
-                чтобы ваш проект начался без рисков и лишних затрат.
-            </h4>
-
-            <h1 className="text-h1 font-bold text-blue text-right">
-                Почему это важно?
-            </h1>
         </section>
     );
 }
