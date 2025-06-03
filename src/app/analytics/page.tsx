f68bfd850117ef0f434ae1a2928fc78cb2e88a35
Lxg1c <teran_2005@mail.ru>
2025-06-03 21:07:02 +0300

    moved sticky info

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -5,6 +5,7 @@ import {
     AnalyticsResultSection,
     AnalyticsDeliverablesSection
 } from "@/widgets/analytic";
+import StickyInfo from "@/shared/ui/stickyInfo/StickyInfo";
 
 
 export default function Analytics() {
@@ -12,6 +13,7 @@ export default function Analytics() {
     return (
         <main className='analytics py-20 space-y-20'>
             <AnalyticsHeroSection />
+            <StickyInfo text="Начнём проект с надёжного фундамента" />
             <AnalyticsStepsSection />
             <AnalyticsResultSection />
             <AnalyticsDeliverablesSection />
--- src/widgets/analytic/analyticsStepsSection/AnalyticsStepsSection.tsx
+++ src/widgets/analytic/analyticsStepsSection/AnalyticsStepsSection.tsx
@@ -2,7 +2,16 @@ import AnalyticsSteps from "@/widgets/analytic/analyticsStepsSection/ui/Analytic
 
 export function AnalyticsStepsSection() {
     return (
-        <section>
+        <section className='container'>
+            <h4 className="font-normal text-h5 max-w-135">
+                Мы анализируем текущее состояние вашего бизнеса, выявляем проблемы и разрабатываем решения,
+                чтобы ваш проект начался без рисков и лишних затрат.
+            </h4>
+
+            <h1 className="text-h1 font-bold text-blue text-right">
+                Почему это важно?
+            </h1>
+
             <AnalyticsSteps />
         </section>
     );
