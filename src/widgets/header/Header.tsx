6dc97818db9ff5638aefb59f821ebebf3bcade24
Lxg1c <teran_2005@mail.ru>
2025-06-04 16:57:06 +0300

    add link to header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import HeaderNav from "@/widgets/header/ui/HeaderNav";
 import Button from "@/shared/ui/button/Button";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header id='header'>
             <div className='header__container container flex justify-between items-center'>
                 <div className='header__logo'>
-                    <Image
-                        className="cursor-pointer"
-                        src="/logo.svg"
-                        alt="Логотип комании"
-                        width={260}
-                        height={80}
-                    />
+                    <Link href="/">
+                        <Image
+                            className="cursor-pointer"
+                            src="/logo.svg"
+                            alt="Логотип комании"
+                            width={260}
+                            height={80}
+                        />
+                    </Link>
                 </div>
 
                 <div className='flex items-center gap-5'>
