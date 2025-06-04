9ef6c62df34934fd976cfa42466c3e01e41f5597
Lxg1c <teran_2005@mail.ru>
2025-06-04 19:14:03 +0300

    moved data

--- src/widgets/analytic/analyticsStepsSection/ui/AnalyticSteps.tsx
+++ src/widgets/analytic/analyticsStepsSection/ui/AnalyticSteps.tsx
@@ -1,30 +1,27 @@
-import { ProcessItem } from "@/widgets/home/homeServiceSection/ui/ui/ProcessItem";
+import {steps} from "@/widgets/analytic/analyticsStepsSection/ui/lib";
+import * as motion from "motion/react-client"
 
-const steps = [
-    { title: "Понимание проблем и возможностей", text: "Мы находим слабые места и точки роста." },
-    { title: "Понимание проблем и возможностей", text: "Мы находим слабые места и точки роста." },
-    { title: "Понимание проблем и возможностей", text: "Мы находим слабые места и точки роста." },
-];
 
 export default function AnalyticsSteps() {
     return (
         <ul>
-            {steps.map((step, i) => (
-                <ProcessItem
-                    key={i}
-                    direction="left"
-                    alt={`${i + 1}`}
-                    style={{ marginLeft: `${i * 150}px` }}
-                    description={
-                        <div className='flex items-center justify-between w-full'>
-                            <h2 className='text-blue mr-8' style={{ fontSize: '64px' }}>{i + 1}</h2>
-                            <div className='w-full'>
-                                <h4 className="font-medium text-h4 text-blue">{step.title}</h4>
-                                <p className="font-normal text-h5">{step.text}</p>
-                            </div>
+            {steps.map((step, index) => (
+                <motion.li
+                    key={index}
+                    initial={{ x: -250, opacity: 0 }}
+                    whileInView={{ x: 40 + 200 * index, opacity: 1 }}
+                    viewport={{ once: true, amount: 0.8 }}
+                    transition={{ duration: 0.8, delay: index * 0.2 }}
+                    className="mb-5"
+                >
+                    <div className='flex items-center gap-5'>
+                        <h2 className='text-blue font-semibold' style={{ fontSize: '64px' }}>{index + 1}</h2>
+                        <div className='w-full'>
+                            <h4 className="font-medium text-h4 mb-1">{step.title}</h4>
+                            <p className="font-normal text-h5">{step.text}</p>
                         </div>
-                    }
-                />
+                    </div>
+                </motion.li>
             ))}
         </ul>
     );
