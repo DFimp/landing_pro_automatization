a453697dce09cb424f348871e82a6c6f5ca060d3
Lxg1c <teran_2005@mail.ru>
2025-06-04 19:14:22 +0300

    refactored by shared comp

--- src/widgets/home/homeServiceSection/ui/ProcessList.tsx
+++ src/widgets/home/homeServiceSection/ui/ProcessList.tsx
@@ -1,55 +1,30 @@
-import {ProcessItem} from "@/widgets/home/homeServiceSection/ui/ui/ProcessItem";
+import { solutions } from "@/widgets/home/homeServiceSection/ui/lib";
+import {AnimatedListItem} from "@/shared/ui/animatedListItem/AnimatedListItem";
 
 
-const ProcessList = () => {
-    return(
+type Props = {
+    descriptionClassName?: string;
+};
+
+const ProcessList = ({ descriptionClassName = "" }: Props) => {
+    return (
         <section className="process-list">
             <div className='process-list__container container'>
                 <ul className='space-y-4'>
-                    <ProcessItem
-                        direction="left"
-                        imageSrc="/process1.svg"
-                        title="Полный цикл внедрения amoCRM"
-                        description="Мы запускаем CRM с нуля: изучаем ваши задачи и цели и настраиваем амо под особенности вашего бизнеса."
-                    />
-
-                    <ProcessItem
-                        direction="right"
-                        imageSrc="/process2.svg"
-                        title="Аналитика и отчеты для управления бизнесом"
-                        description="Настраиваем аналитику в amoCRM, чтобы вы могли отслеживать ключевые показатели и принимать обоснованные решения."
-                        offset="ml-[250px]"
-                    />
-
-                    <ProcessItem
-                        direction="left"
-                        imageSrc="/process3.svg"
-                        title="Сопровождение и развитие системы"
-                        description="Мы не просто поддерживаем amoCRM, но и развиваем её,
-                                  добавляя новые возможности и адаптируя под ваши задачи."
-                    />
-
-                    <ProcessItem
-                        direction="right"
-                        imageSrc="/process4.svg"
-                        title="Интеграция amoCRM с любыми необходимыми сервисами"
-                        description="Соединяем amoCRM с почтовыми сервисами, мессенджерами,
-                                  телефонией, бухгалтерией и другими платформами, чтобы упростить вашу работу и объединить
-                                  все инструменты в единую систему."
-                        offset="ml-[250px]"
-                    />
-
-                    <ProcessItem
-                        direction="left"
-                        imageSrc="/process5.svg"
-                        title="Подключение и настройка виджетов для amoCRM"
-                        description="Сокращаем рутину, оптимизируем процессы, при помощи
-                                  готовых виджетов."
-                    />
+                    {solutions.map((solution, index) => (
+                        <AnimatedListItem
+                            key={index}
+                            index={index}
+                            imageSrc={solution.imageSrc}
+                            title={solution.title}
+                            description={solution.description}
+                            descriptionClassName={descriptionClassName}
+                        />
+                    ))}
                 </ul>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default ProcessList;
