368394409c0a3a4e0abe16ab1776fcbc4fb2dfb7
Lxg1c <teran_2005@mail.ru>
2025-06-04 19:11:18 +0300

    moved data fixed animation

--- src/widgets/analytic/analyticsResultSection/ui/AnalyticsResult.tsx
+++ src/widgets/analytic/analyticsResultSection/ui/AnalyticsResult.tsx
@@ -1,65 +1,22 @@
 'use client'
 
 import Image from "next/image";
-import { motion } from "framer-motion";
-import { useRef } from "react";
-import { useInView } from "framer-motion";
-
-const services = [
-    {
-        img: "/service1.svg",
-        title: "Сбор информации",
-        items: [
-            "Изучаем ваш бизнес и цели.",
-            "Анализируем текущие процессы и технологии.",
-            "Общаемся с ключевыми сотрудниками."
-        ]
-    },
-    {
-        img: "/service2.svg",
-        title: "Поиск проблем",
-        items: [
-            "Определяем слабые места в работе.",
-            "Оцениваем риски и их влияние.",
-            "Сравниваем с конкурентами."
-        ]
-    },
-    {
-        img: "/service3.svg",
-        title: "Разработка решений",
-        items: [
-            "Готовим техническое задание (ТЗ).",
-            "Предлагаем оптимизации.",
-            "Устанавливаем ключевые показатели успеха."
-        ]
-    },
-    {
-        img: "/service4.svg",
-        title: "План действий",
-        items: [
-            "Создаём пошаговый план работ.",
-            "Расчёт времени и бюджета.",
-            "Учитываем особенности вашего бизнеса."
-        ]
-    }
-];
+import * as motion from "motion/react-client"
+import {services} from "@/widgets/analytic/analyticsResultSection/ui/lib";
 
 export default function AnalyticsResult() {
-    const containerRef = useRef(null);
-    const inView = useInView(containerRef, { once: true, threshold: 0.2 });
-
     return (
         <ul
-            ref={containerRef}
             className='bg-black rounded-4xl grid grid-cols-2 gap-20 py-20 px-53'
         >
-            {services.map((service, idx) => (
+            {services.map((service, index) => (
                 <motion.li
-                    key={idx}
+                    key={index}
                     className='flex items-end justify-center gap-12'
                     initial={{ opacity: 0, y: 30 }}
-                    animate={inView ? { opacity: 1, y: 0 } : {}}
-                    transition={{ duration: 0.6, delay: idx * 0.3 }}
+                    whileInView={{opacity: 1, y: 30}}
+                    viewport={{once: true, amount: 0.6}}
+                    transition={{ duration: 0.6, delay: index * 0.3 }}
                 >
                     <Image src={service.img} alt={service.title} width={80} height={80} />
                     <div className='contenet space-y-5'>
