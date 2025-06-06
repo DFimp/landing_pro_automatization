a0f0d38ddb38ed72484ef6433adbec15ca9d45fe
Lxg1c <teran_2005@mail.ru>
2025-06-06 16:07:30 +0300

    height to 650px

--- src/widgets/accompaniment/AccompanimentProblemsSection/ui/ProblemsGrid.tsx
+++ src/widgets/accompaniment/AccompanimentProblemsSection/ui/ProblemsGrid.tsx
@@ -14,7 +14,7 @@ export default function ProblemsGrid() {
                         whileInView={{ opacity: 1, y: 0 }}
                         viewport={{ once: true, amount: 0.6 }}
                         transition={{ duration: 0.5, delay: index * 0.2 }}
-                        className="card border-1 rounded-3xl px-10 py-15 space-y-3 max-w-90 h-[600px]"
+                        className="card border-1 rounded-3xl px-10 py-15 space-y-3 max-w-90 h-[650px]"
                     >
                         <Image src={problem.image} alt="Проблема и ее решение" width={92} height={94} />
                         <h3 className="text-white font-medium text-h5">{problem.title}</h3>
