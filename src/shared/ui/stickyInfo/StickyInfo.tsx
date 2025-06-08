d91bf0a9691cf0f1a8a0d915e60bd3a849b0d3d9
DFimp <ilia.fimp@gmail.com>
2025-06-08 12:09:51 +0300

    seo optimization

--- src/shared/ui/stickyInfo/StickyInfo.tsx
+++ src/shared/ui/stickyInfo/StickyInfo.tsx
@@ -1,23 +1,28 @@
 type StickyInfoProps = {
-    text: string
-    left?: boolean
-    className?: string
-}
+  text: string;
+  left?: boolean;
+  className?: string;
+};
 
 export default function StickyInfo({
-                                       text,
-                                       left = true,
-                                       className = "",
+  text,
+  left = true,
+  className = "",
 }: StickyInfoProps) {
-    return (
-        <div className="sticky-info h-10 w-full">
-            <div className="relative h-20">
-                <div
-                    className={`absolute bg-blue top-0 text-white text-h4 font-normal py-4 px-10 pr-32 ${className} 
-                    ${left ? "left-0 rounded-r-full pl-50" : "right-0 rounded-l-full pr-50"}`}>
-                    {text}
-                </div>
-            </div>
+  return (
+    <div className="sticky-info h-10 w-full">
+      <div className="relative h-20">
+        <div
+          className={`absolute bg-blue top-0 text-white text-h4 font-normal py-4 px-10 pr-32 ${className} 
+            ${
+              left
+                ? "left-0 rounded-r-full pl-50"
+                : "right-0 rounded-l-full pr-50"
+            }`}
+        >
+          {text}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
