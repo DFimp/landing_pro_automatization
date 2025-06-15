82a466d3c13920e13fa163af37f363ce82bd7f48
DFimp <ilia.fimp@gmail.com>
2025-06-15 21:32:39 +0300

    fix

--- src/app/widgets/telegram-widget/layout.tsx
+++ src/app/widgets/telegram-widget/layout.tsx
@@ -1,21 +1,16 @@
 // app/telegram-widget/layout.tsx
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Телеграм уведомления amoCRM",
-    description: "Получайте мгновенные уведомления в Telegram и запускайте SalesBot одним нажатием. Полная автоматизация коммуникаций с клиентами.",
-    keywords: "AmoCRM, Telegram, Телеграм, уведомления, SalesBot, автоматизация, CRM, интеграция",
-    openGraph: {
-        title: "Телеграм уведомления amoCRM",
-        description: "Получайте мгновенные уведомления в Telegram и запускайте SalesBot одним нажатием.",
-        type: "website",
-    },
+  title: "Телеграм уведомления amoCRM",
+  description:
+    "Получайте мгновенные уведомления в Telegram и запускайте SalesBot одним нажатием. Полная автоматизация коммуникаций с клиентами.",
 };
 
 export default function TelegramWidgetLayout({
-                                                 children,
-                                             }: {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return <>{children}</>;
+  return <>{children}</>;
 }
