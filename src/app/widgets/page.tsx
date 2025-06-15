cb8fbf270b8624ab9b85a00de456657a6763ecc3
DFimp <ilia.fimp@gmail.com>
2025-06-15 20:46:18 +0300

    add widget page

--- src/app/widgets/page.tsx
+++ src/app/widgets/page.tsx
@@ -1,7 +1,75 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Widgets() {
-    return (
-        <main id="widgets" className="pb-20 pt-20">
-                        
-        </main>
-    );
+  const router = useRouter();
+
+  return (
+    <main id="widgets" className="container pb-20 pt-20">
+      <section className="mt-20 grid grid-cols-2 gap-8">
+        <div
+          onClick={() => router.push("/widgets/telegram-widget")}
+          className="cursor-pointer bg-white p-8 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-500 border border-gray-100 hover:transform hover:scale-105 animate-fade-in-up"
+          style={{
+            boxShadow:
+              "0 10px 25px -5px rgba(0, 0, 0, 0.2), 0 10px 16px -8px rgba(0, 0, 0, 0.15)",
+          }}
+        >
+          <div className="flex flex-row">
+            <span className="text-2xl">💬</span>
+            <h1 className="text-xl font-bold text-gray-900 mb-3">
+              Telegram Виджет для AmoCRM
+            </h1>
+          </div>
+
+          <p>
+            Получайте мгновенные уведомления в Telegram и запускайте SalesBot
+            одним нажатием. Полная автоматизация коммуникаций с клиентами прямо
+            в мессенджере.
+          </p>
+        </div>
+        <div
+          onClick={() => router.push("/widgets/auto-tasks-widget")}
+          className="cursor-pointer bg-white p-8 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-500 border border-gray-100 hover:transform hover:scale-105 animate-fade-in-up"
+          style={{
+            boxShadow:
+              "0 10px 25px -5px rgba(0, 0, 0, 0.2), 0 10px 16px -8px rgba(0, 0, 0, 0.15)",
+          }}
+        >
+          <div className="flex flex-row">
+            <span className="text-2xl">⚡</span>
+            <h1 className="text-xl font-bold text-gray-900 mb-3">
+              Автозадачи для AmoCRM
+            </h1>
+          </div>
+
+          <p>
+            Автоматически создавайте задачи для сделок без активности.
+            Настраиваемые статусы, типы задач и ответственные. Увеличьте
+            конверсию на 35% благодаря своевременному контакту с клиентами.
+          </p>
+        </div>
+        <div
+          onClick={() => router.push("/widgets/dublicate-leads")}
+          className="cursor-pointer bg-white p-8 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-500 border border-gray-100 hover:transform hover:scale-105 animate-fade-in-up"
+          style={{
+            boxShadow:
+              "0 10px 25px -5px rgba(0, 0, 0, 0.2), 0 10px 16px -8px rgba(0, 0, 0, 0.15)",
+          }}
+        >
+          <div className="flex flex-row">
+            <h1 className="text-xl font-bold text-gray-900 mb-3">
+              Дубли сделок
+            </h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            deserunt quaerat fugiat eius, distinctio est, error, earum neque
+            quos necessitatibus excepturi.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
