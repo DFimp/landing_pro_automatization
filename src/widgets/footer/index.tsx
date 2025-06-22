f8cac4cf10ccbc2457a4e663fc0808568f3c9fe6
DFimp <ilia.fimp@gmail.com>
2025-06-23 01:09:00 +0300

    fix footer

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
   const { isIframe } = hiddenInIframe();
 
   return (
-    <footer id={isIframe ? "hidden-in-iframe" : ""} className="bg-black">
+    <footer id={isIframe ? "hidden-in-iframe" : ""} className="bg-[#1e1e1e]">
       <div className="max-w-[1200px] m-auto py-10 flex items-center justify-between">
         <Link href="/">
           <Image
@@ -20,9 +20,15 @@ export const Footer = () => {
           />
         </Link>
 
-        <Link href={"/privacy"} className="text-h7 text-blue">
-          Политика конфиденциальности
-        </Link>
+        <div className="text-h7 text-blue flex flex-col items-end gap-1">
+          <Link href={"/privacy"}>
+            Политика конфиденциальности
+          </Link>
+
+          <Link href={"https://t.me/@proavtomatization_support_bot"} target="_blank">
+            Написать в поддержку
+          </Link>
+        </div>
       </div>
     </footer>
   );
