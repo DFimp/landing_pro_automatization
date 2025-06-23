04ef0ccd19918f3e6134ac663606520e5133b14f
DFimp <ilia.fimp@gmail.com>
2025-06-23 08:07:22 +0300

    wip privacyPage

--- src/page/PrivacyPage/ui/Point.tsx
+++ src/page/PrivacyPage/ui/Point.tsx
@@ -1,11 +1,12 @@
 interface IPoint {
   title: string,
   description: string,
+  className?: string,
 }
 
-export const Point: React.FC<IPoint> = ({title, description}) => {
+export const Point: React.FC<IPoint> = ({title, description, className=""}) => {
   return (
-    <p className="text-h7 text-gray leading-relaxed mb-3">
+    <p className={"text-h7 text-gray leading-relaxed mb-3" + " " + className}>
       <strong>{title}</strong>
       {description}
     </p>
