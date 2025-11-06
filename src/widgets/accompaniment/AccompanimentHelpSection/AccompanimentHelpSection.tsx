4caa0c2395ef2f0223c7e1beb3cbc8c47d83f12d
DFimp <ilia.fimp@gmail.com>
2025-11-06 23:36:22 +0300

    fix accompaniment

--- src/widgets/accompaniment/AccompanimentHelpSection/AccompanimentHelpSection.tsx
+++ src/widgets/accompaniment/AccompanimentHelpSection/AccompanimentHelpSection.tsx
@@ -2,14 +2,10 @@ import { TitleArrow } from "@/shared/ui/titleArrow/TitleArrow";
 import HelpServices from "@/widgets/accompaniment/AccompanimentHelpSection/ui/HelpServices";
 
 export function AccompanimentHelpSection() {
-    return (
-        <div className='flex flex-col gap-20 mt-20'>
-            <TitleArrow
-                text='Как мы можем помочь?'
-                link='/'
-                variant='secondary'
-            />
-            <HelpServices />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-20 mt-20">
+      <TitleArrow text="Как мы можем помочь?" variant="secondary" />
+      <HelpServices />
+    </div>
+  );
 }
--- src/widgets/accompaniment/AccompanimentProblemsSection/AccompanimentProblemsSection.tsx
+++ src/widgets/accompaniment/AccompanimentProblemsSection/AccompanimentProblemsSection.tsx
@@ -2,16 +2,15 @@ import { TitleArrow } from "@/shared/ui/titleArrow/TitleArrow";
 import ProblemsGrid from "@/widgets/accompaniment/AccompanimentProblemsSection/ui/ProblemsGrid";
 
 export function AccompanimentProblemsSection() {
-    return (
-        <section className='flex flex-col sm:gap-20 gap-10'>
-            <TitleArrow
-                className='sm:mt-40 mt-10 sm:text-2xl text-[14px]'
-                text='Вот часть распространенных проблем, которые влияют на качество работы с клиентами'
-                link='/'
-                variant='secondary'
-                bold={false}
-            />
-            <ProblemsGrid />
-        </section>
-    );
+  return (
+    <section className="flex flex-col sm:gap-20 gap-10">
+      <TitleArrow
+        className="sm:mt-40 mt-10 sm:text-2xl text-[14px]"
+        text="Вот часть распространенных проблем, которые влияют на качество работы с клиентами"
+        variant="secondary"
+        bold={false}
+      />
+      <ProblemsGrid />
+    </section>
+  );
 }
