7eba447066be2605c34719e4a6abc5670ca5adea
DFimp <118912553+DFimp@users.noreply.github.com>
2026-03-11 14:06:01 +0300

    Merge branch 'fix_worksteps' into 'main'
    * new animations
    
    * dragging with the mouse
    
    ---------
    
    Co-authored-by: IlonMaxi <108355205+IlonMaxi@users.noreply.github.com>

--- src/widgets/home/homeStepsSection/ui/WorkSteps.tsx
+++ src/widgets/home/homeStepsSection/ui/WorkSteps.tsx
@@ -1,30 +1,10 @@
-import { Card } from "@/shared/ui/Card/Card";
 import { steps } from "@/widgets/home/homeStepsSection/ui/lib";
-import WorkStepsScrollStory from "./WorkStepsScrollStory.client";
+import WorkStepsCarousel from "./WorkStepsCarousel.client";
 
 export default function WorkSteps() {
   return (
     <div className="w-full mb-20 sm:mt-20 mt-10">
-      <div className="hidden sm:block">
-        <WorkStepsScrollStory steps={steps} />
-      </div>
-
-      <div className="sm:hidden">
-        <ul className="flex flex-col gap-4 select-none">
-          {steps.map((step, index) => (
-            <Card
-              variant="expandable"
-              key={index}
-              index={index}
-              content={step.content}
-              description={step.description}
-              result={step.result}
-              isFirst={index === 0}
-              isLast={index === steps.length - 1}
-            />
-          ))}
-        </ul>
-      </div>
+      <WorkStepsCarousel steps={steps} />
     </div>
   );
 }
